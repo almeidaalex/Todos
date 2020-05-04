8fdb0f437919b511774a517dfb30bbbb35597601
Alex Almeida <almeida.alex@outlook.com>
2020-05-04 10:05:49 -0300

    Some changes to save a collection of todos, not finished yet

--- TodoBasic/Program.cs
+++ TodoBasic/Program.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System;
 
 namespace Todos
 {
@@ -16,6 +19,7 @@ namespace Todos
             app.MapGet("/api/todos/{id}", GetTodo);
             app.MapPost("/api/todos", CreateTodo);
             app.MapPost("/api/todos/{id}", UpdateCompleted);
+            app.MapPut("/api/todos/", UpdateTodos);
             app.MapDelete("/api/todos/{id}", DeleteTodo);
 
             await app.RunAsync();
@@ -57,6 +61,46 @@ namespace Todos
             await db.SaveChangesAsync();
 
             context.Response.StatusCode = 204;
+        }
+
+        static async Task UpdateTodos(HttpContext context)
+        {
+            var todos = await context.Request.ReadJsonAsync<IEnumerable<Todo>>();
+
+            using var db = new TodoDbContext();
+            var others = await db.Todos.ToListAsync();
+
+            var toRemove = others.Except(todos);
+            db.Todos.RemoveRange(toRemove);
+
+            var toAdd = todos.Except(others);            
+            await db.Todos.AddRangeAsync(toAdd);
+
+            var toUpdate = todos.Intersect(others);
+
+            await db.SaveChangesAsync();
+            context.Response.StatusCode = 204;
+        }
+
+
+        static async Task UpdateAndAdd(HttpContext context)
+        {
+             if (!context.Request.RouteValues.TryGet("id", out int id))
+            {
+                context.Response.StatusCode = 400;
+                return;
+            }
+
+            using var db = new TodoDbContext();
+            var updateTodo = await db.Todos.FindAsync(id);
+            updateTodo.Name = $"Update Task at {DateTime.Now}";
+
+            var newTodo = new Todo{ Name = "New task" };        
+            db.Add(newTodo);
+
+            await db.SaveChangesAsync();
+
+            context.Response.StatusCode = 200;
         }
 
         static async Task UpdateCompleted(HttpContext context)
--- TodoBasic/Todo.cs
+++ TodoBasic/Todo.cs
@@ -1,8 +1,11 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 namespace Todos
 {
-    public class Todo
+    public class Todo : IEquatable<Todo>
     {
         [JsonPropertyName("id")]
         public int Id { get; set; }
@@ -12,5 +15,14 @@ namespace Todos
 
         [JsonPropertyName("isComplete")]
         public bool IsComplete { get; set; }
+
+        public bool Equals([DisallowNull] Todo other)
+        {
+            return this.Id == other.Id;
+        }
+
+        public override bool Equals(object obj) => Equals(obj as Todo);
+
+        public override int GetHashCode() => Id.GetHashCode();
     }
 }
